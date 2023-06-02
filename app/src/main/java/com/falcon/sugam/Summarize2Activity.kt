3f6799f1ec176ae89aba0cfb7a73abbfb2e562a3
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-06-02 06:54:31 +0530

    doing

--- app/src/main/java/com/falcon/sugam/Summarize2Activity.kt
+++ app/src/main/java/com/falcon/sugam/Summarize2Activity.kt
@@ -1,5 +1,6 @@
 package com.falcon.sugam
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +11,8 @@ import org.json.JSONObject
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
 import android.widget.ImageView
 import android.widget.Toast
@@ -18,6 +21,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import com.falcon.sugam.databinding.ActivitySummarize2Binding
 import com.falcon.sugam.databinding.ActivitySummarizeBinding
 import java.util.*
+import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Local
 
 var url = "http://34.171.182.83/upload"
 
@@ -27,6 +31,8 @@ class Summarize2Activity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivitySummarize2Binding
     lateinit var tts: TextToSpeech
+
+    private val RQ_SPEECH_RC = 102
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -52,11 +58,26 @@ class Summarize2Activity : AppCompatActivity() {
         }
         findViewById<TextView>(R.id.summarizedText).text
         getJson(findViewById<TextView>(R.id.summarizedText).text.toString())
+        binding.micButton.setOnClickListener {
+            askSpeechInput()
+        }
     }
+
+    private fun askSpeechInput() {
+        if (!SpeechRecognizer.isRecognitionAvailable(this)) {
+            Toast.makeText(this, "bk madarchod, deice he chutiya hai tera", Toast.LENGTH_SHORT).show()
+        } else {
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT, "SAY SOMETHING BRUH")
+            startActivityForResult(i, RQ_SPEECH_RC)
+        }
+    }
+
     private fun getJson(text: String) {
         url = "$url?text=$text"
         Log.i("cattttt", url)
-//        Toast.makeText(this, "CAT", Toast.LENGTH_SHORT).show()
         Fuel.get(url)
             .responseString { _, response, result ->
                 when (result) {
@@ -70,16 +91,6 @@ class Summarize2Activity : AppCompatActivity() {
                         Handler(Looper.getMainLooper()).post {
                             binding.summarizedText.text = textValue
                         }
-//                        try {
-//                            Toast.makeText(this, resultJson.getString("text"), Toast.LENGTH_SHORT).show()
-//                            binding.testview.text = textValue
-//                            binding.summarizedText.text = textValue
-//                        }
-//                        catch (e: java.lang.Exception) {
-//                            Log.i("errorrrr", e.message.toString())
-//                        }
-                        Log.i("happyhappyhappy", textValue)
-                        Log.i("happyhappyid", idValue)
                     }
                     is Result.Failure -> {
                         val error = result.getException()
@@ -89,5 +100,13 @@ class Summarize2Activity : AppCompatActivity() {
                     }
                 }
             }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RQ_SPEECH_RC && resultCode == Activity.RESULT_OK) {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            Toast.makeText(this, result?.get(0).toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
